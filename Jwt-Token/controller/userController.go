be9277a6fdae09c58a3a9bb7d91f85d8b66f9702
NishanthGit-Golang <nishanth001.linkedin@gmail.com>
2024-07-02 23:39:06 +0530

    Update userController.go

---
+++ Jwt-Token/controller/userController.go
@@ -0,0 +1,19 @@
+package controller
+
+import "fmt"
+
+func SignUp() {
+	fmt.Println("Calling SignUp Function")
+}
+
+func Login() {
+	fmt.Println("Calling Login Function")
+}
+
+func GetUsers() {
+	fmt.Println("Calling GetUsers Function")
+}
+
+func GetUserByID() {
+	fmt.Println("Calling GetUserByID Function")
+}
